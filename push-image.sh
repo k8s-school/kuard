7f4b275529973cf7a3daf40893cd153224fc8a7e
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-11-24 17:00:44 +0100

    Update current feature

--- push-image.sh
+++ push-image.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Create docker image containing kops tools and scripts
 
-# @author  Fabrice Jammes
-
-set -euxo pipefail
-
-DIR=$(cd "$(dirname "$0")"; pwd -P)
-
-. $DIR/conf.sh
-
-docker push "$IMAGE"
+make all-push
